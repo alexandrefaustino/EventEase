25b4c8b81f671baf37f38047f9ae30b51878ec6b
Alexandre Faustino <alexandrefaculdade@gmail.com>
2025-07-06 11:07:40 -0300

    Versão inicial do projeto EventEase

---
+++ src/EventEase.Web/Models/EventModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventEase.Web.Models;
+
+public class EventModel
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    [Required(ErrorMessage = "O título é obrigatório.")]
+    public string Title { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "A data é obrigatória.")]
+    public DateTime Date { get; set; } = DateTime.Today;
+
+    [Required(ErrorMessage = "O local é obrigatório.")]
+    public string Location { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+}
